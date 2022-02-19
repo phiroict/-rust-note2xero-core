05051d085d2cc55d690e4631a559321c0332f320
Philip Rodrigues <phil@phiroict.co.nz>
2022-02-19 14:43:04 +1300

    Changed default due date to 7 and updated build container versions.

--- src/constants.rs
+++ src/constants.rs
@@ -3,6 +3,6 @@ pub const TAX_RATE: &str = "15% GST on Income";
 pub const XERO_INCOME_ACCOUNT_NUMBER: &str = "200";
 pub const XERO_INVENTORY_ACCOUNT_NUMBER: &str = "217";
 pub const CURRENCY: &str = "NZD";
-pub const INVOICE_DAYS_TO_PAY: i32 = 30;
+pub const INVOICE_DAYS_TO_PAY: i32 = 7;
 pub const INVOICE_DAYS_TODAY: i32 = 0;
 pub const BRANDING_THEME: &str = "Standard";
